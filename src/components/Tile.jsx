3fe44a2bd48a7ee977210743c7878e863f9fe981
Mihir Thakur <mihirthakur5@gmail.com>
2024-05-07 14:56:40 +0530

    for-mobile-devices

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -7,9 +7,9 @@ const Tile = ({ className, value, onClick, playerTurn }) => {
   }
 
   return (
-    <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>
+    <button onClick={onClick} className={`tile ${className} ${hoverClass}`}>
       {value}
-    </div>
+    </button>
   );
 };
 
